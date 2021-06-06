c35ec1d7ff3d169f1710e81b4e7d3848217593b0
Shezan <baigshezanx@gmail.com>
2021-06-06 09:35:44 +0530

    Committing the required files

---
+++ main.py
@@ -0,0 +1,29 @@
+from logging import debug
+from flask import Flask , render_template,request
+from sklearn.linear_model import LinearRegression
+import joblib
+
+model = joblib.load('hiring_model.pkl')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template('base.html')
+
+
+@app.route('/predict' , methods = ['POST'])
+def predict():
+
+    exp = request.form.get('experience')
+    score = request.form.get('test_score')
+    interview_score = request.form.get('interview_score')
+    prediction = model.predict([[int(exp) , int(score) , int(interview_score)]])
+
+    output = round(prediction[0] , 2)
+
+    return render_template('base.html' , prediction_text = f"Employee Salary will be $ {output}")
+
+    
+app.run(debug=True)
